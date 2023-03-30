b4182bb7038c682da5cc8855791ac0536580272b
Marc Gonzalez <marca_gonzalez@hotmail.com>
2023-03-29 20:34:40 -0400

    New

---
+++ algorithm.js
@@ -0,0 +1,9 @@
+
+function add_function(){
+    var numb1 = document.getElementById("num1").value;
+    var numb2 = document.getElementById("num2").value;   
+   
+    sum = numb1 * numb2;
+   document.getElementById("num3").value = (sum);
+
+}
