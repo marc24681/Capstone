9a24fa622c310947b63ace54d9ab0210cd01e225
Marc Gonzalez <marca_gonzalez@hotmail.com>
2023-05-31 16:07:39 -0400

    Version_1

--- algorithm.js
+++ algorithm.js
@@ -1,27 +1,47 @@
+const form = document.getElementById('questionnaire');
+const resultElement = document.getElementById('result');
 
-function getValue() {
-    // Get the radio buttons
-   
-    const q1 = parseInt(document.querySelector('input[name="question1"]:checked').value);
-	const q2 = parseInt(document.querySelector('input[name="question2"]:checked').value);
-    const q3 = parseInt(document.querySelector('input[name="question3"]:checked').value);
-	const q4 = parseInt(document.querySelector('input[name="question4"]:checked').value);
-    const q5 = parseInt(document.querySelector('input[name="question5"]:checked').value);
-	const q6 = parseInt(document.querySelector('input[name="question6"]:checked').value);
-    const q7 = parseInt(document.querySelector('input[name="question7"]:checked').value);
-	const q8 = parseInt(document.querySelector('input[name="question8"]:checked').value);
-    const q9 = parseInt(document.querySelector('input[name="question9"]:checked').value);
-	const q10 = parseInt(document.querySelector('input[name="question10"]:checked').value);
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	const q1 = parseInt(form.elements['q1'].value);
+	const q2 = parseInt(form.elements['q2'].value);
+    const q3 = parseInt(form.elements['q3'].value);
+	const q4 = parseInt(form.elements['q4'].value);
+    const q5 = parseInt(form.elements['q5'].value);
+    const q6 = parseInt(form.elements['q6'].value);
+    const q7 = parseInt(form.elements['q7'].value);
+    const q8 = parseInt(form.elements['q8'].value); 
+    const q9 = parseInt(form.elements['q9'].value);
+    const q10 = parseInt(form.elements['q10'].value);
+
+	const total = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 +q10;
+
+	if (total > 20) {
+		resultElement.innerText = 'You are in danger.';
+		resultElement.classList.add('danger');
+	} else {
+		resultElement.innerText = 'You are safe.';
+		resultElement.classList.add('safe');
+	}
+
+	form.style.display = 'none';
+	fadeIn(resultElement);
+
+    function fadeIn(element) {
+        element.style.display = 'block';
+        const opacity = parseFloat(window.getComputedStyle(element).getPropertyValue('opacity'));
     
-			
-    const totalScore = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 +q10;
+        let currentOpacity = 0;
+        const intervalId = setInterval(function() {
+            currentOpacity += 0.1;
+            element.style.opacity = currentOpacity;
     
-    if (totalScore > 20){
-        document.getElementById('score').innerHTML = "You are in danger";
-    } else
-    {
-        document.getElementById('score').innerHTML = "You are safe";
+            if (currentOpacity >= opacity) {
+                clearInterval(intervalId);
+            }
+        }, 50);
     }
-      
-    
-}
+
+});
+
--- store.js
+++ store.js
@@ -1,26 +1,11 @@
 function submit() {
-    var form = document.getElementById("questions");
-    var answer1 = parseInt(form.elements["q1"].value);
-    localStorage.setItem("q1", answer1);
-    var answer2 = parseInt(form.elements["q2"].value);
-    localStorage.setItem("q2", answer2);
-    var answer3 = parseInt(form.elements["q3"].value);
-    localStorage.setItem("q3", answer3);
-    var answer4 = parseInt(form.elements["q4"].value);
-    localStorage.setItem("q4", answer4);
-    var answer5 = parseInt(form.elements["q5"].value);
-    localStorage.setItem("q5", answer5);
-    var answer6 = parseInt(form.elements["q6"].value);
-    localStorage.setItem("q6", answer6);
-    var answer7 = parseInt(form.elements["q7"].value);
-    localStorage.setItem("q7", answer7);
-    var answer8 = parseInt(form.elements["q8"].value);
-    localStorage.setItem("q8", answer8);
-    var answer9 = parseInt(form.elements["q9"].value);
-    localStorage.setItem("q9", answer9);
-    var answer10 = parseInt(form.elements["q10"].value);
-    localStorage.setItem("q10", answer10);
-
-    window.location.href = "answer.html";
+  const resultElement = document.getElementById('score');
+  const totalScore = parseInt(localStorage.getItem('totalScore'));
+  
+  if (totalScore > 20) {
+    resultElement.innerText = 'You are in danger.';
+  } else {
+    resultElement.innerText = 'You are safe.';
+  }
   }
   
